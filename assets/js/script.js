f9476a1ca3ad5e16625d032f3a4a724a99556415
Daniel Grazziotti <dgplays14@gmail.com>
2021-10-15 05:50:32 -0300

    added animation on mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
-const menuMobile = document.querySelector('.js-menu-mobile img')
+const hamburgerIcon = document.querySelector('.js-hamburger')
 const mainMenu = document.querySelector('.header__main-menu')
+const mainMenuList = document.querySelector('.header__main-menu__list')
 const headerContent = document.querySelector('.header__content')
 
-const toggleMenu = () => {
-    headerContent.classList.toggle('is-fixed')
-    mainMenu.classList.toggle('is-open')
-    mainMenu.classList.contains('is-open')
-        ? menuMobile.src = './assets/images/icon-close.svg'
-        : menuMobile.src = './assets/images/icon-hamburger.svg'
+
+const openMenu = () => {
+    mainMenu.classList.add('is-open')
+    setTimeout( () => {
+        mainMenu.classList.add('animate')
+        mainMenuList.classList.add('animate')
+    }, 1)
+    headerContent.classList.add('is-fixed')
+    hamburgerIcon.classList.add('header__hamburger--open')
 }
 
-menuMobile.addEventListener('click', toggleMenu)
+const closeMenu = () => {
+    mainMenu.classList.remove('animate')
+    mainMenuList.classList.remove('animate')
+    setTimeout( () => {
+        mainMenu.classList.remove('is-open')
+    }, 200)
+    headerContent.classList.remove('is-fixed')
+    hamburgerIcon.classList.remove('header__hamburger--open')
+}
+
+const isMenuOpen = () => mainMenu.classList.contains('is-open')
+
+const toggleMenu = () => {
+    isMenuOpen()
+        ? closeMenu()
+        : openMenu()
+}
+
+hamburgerIcon.addEventListener('click', toggleMenu)
